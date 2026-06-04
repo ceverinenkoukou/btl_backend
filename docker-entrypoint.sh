b2c10c0ede10d9edfa08cd80b10c247614eb0fe8
Cevérine Nkoukou <ceverine.nkoukou@ez-audiovisuel.com>
2026-06-04 20:04:04 +0100

    fixe entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,25 @@
 set -e
 
 echo "Attente de la base de données PostgreSQL..."
-while ! pg_isready -h ${POSTGRES_HOST:-db} -p ${POSTGRES_PORT:-5432} -U ${POSTGRES_USER:-postgres}; do
-  sleep 1
-done
+
+# Si la variable DATABASE_URL est définie (Production sur Railway)
+if [ -n "$DATABASE_URL" ]; then
+  while ! pg_isready -d "$DATABASE_URL"; do
+    echo "PostgreSQL (via DATABASE_URL) n'est pas encore prêt..."
+    sleep 1
+  done
+else
+  # Sinon, on utilise la méthode classique (Local avec Docker Compose)
+  while ! pg_isready -h ${POSTGRES_HOST:-db} -p ${POSTGRES_PORT:-5432} -U ${POSTGRES_USER:-postgres}; do
+    echo "PostgreSQL local n'est pas encore prêt..."
+    sleep 1
+  done
+fi
 
 echo "PostgreSQL est prêt!"
 
-
-# echo "Application des migrations de la base de données..."
+# Application des migrations de la base de données
 python manage.py migrate --noinput
-
-# echo "Verification/Creation du superutilisateur par defaut..."
-# python manage.py create_default_admin
 
 echo "Démarrage de l'application..."
 exec "$@"
