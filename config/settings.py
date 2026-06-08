0753d195b25d4a334effc654e43b409bcc32abc1
Cevérine Nkoukou <ceverine.nkoukou@ez-audiovisuel.com>
2026-06-08 18:29:14 +0100

    5settings

--- config/settings.py
+++ config/settings.py
@@ -153,8 +153,9 @@ EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.conso
 if config('GMAIL_SMTP_USER', default=''):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST_USER = config('GMAIL_SMTP_USER')
     EMAIL_HOST_PASSWORD = config('GMAIL_SMTP_PASSWORD')
 
